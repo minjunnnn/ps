1dd18ae40972cb27273db5e0587cd397c3df2599
violet-mj <violet_prog@naver.com>
2023-04-03 14:29:46 +0900

    koreaxyonsei baekjoon contest 4/2

---
+++ baekjoon/contest/2023/koreaxyonsei/a.py
@@ -0,0 +1,25 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def solve():
+  s = input()
+
+  KOREA = 'KOREA'
+  YONSEI = 'YONSEI'
+
+  k = 0
+  y = 0
+
+  for i in s:
+    if i == KOREA[k]:
+       k += 1
+       if k == 5:
+          return KOREA
+    
+    if i == YONSEI[y]:
+       y += 1
+       if y == 6:
+          return YONSEI
+    
+if __name__ == "__main__":
+    print(solve())
---
+++ baekjoon/contest/2023/koreaxyonsei/c.py
@@ -0,0 +1,54 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def check(v):
+  global t, k
+  tired = 0
+  for i in t:
+    flag = True
+    for j in i:
+      if j > v:
+        flag = False
+    
+    if not flag:
+      tired += 1
+  if tired > k:
+    return False
+  else:
+    return True
+
+def solve():
+  global t, k
+  n, k = map(int, input().split())
+  L = [*map(int, input().split())]
+  t = []
+
+  if n == 1:
+    return 0
+
+  for idx, i in enumerate(L):
+    if idx == 0:
+      t.append([abs(L[idx + 1] - L[idx])])
+    elif idx == n-1:
+      t.append([abs(L[idx] - L[idx-1])])
+    else:
+      t.append((abs(L[idx+1] - L[idx]), abs(L[idx] - L[idx-1])))
+  l = 0
+  max_ = -1
+  for i in t:
+    for j in i:
+      max_ = max(max_, j)
+  r = max_
+  while l + 1 < r:
+    mid = (l + r) >> 1
+    if check(mid):
+      r = mid
+    else:
+      l = mid
+  if check(l):
+    return l
+  else:
+    return r
+    
+if __name__ == "__main__":
+    print(solve())

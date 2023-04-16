ccd3903b8fdf24507cfab75d1e0f7283c3288be9
minjunnnn <violetprog@gmail.com>
2023-04-16 19:55:43 +0900

    sasa's 4 dimension candy box solved

---
+++ baekjoon/contest/2023/waku/j.py
@@ -0,0 +1,24 @@
+import sys
+from bisect import bisect_right, bisect_left
+input = lambda: sys.stdin.readline().rstrip()
+output = sys.stdout.write
+
+def solve():
+    n, m = map(int, input().split())
+    A = [*map(int, input().split())]
+    B = [int(input()) for i in range(n)]
+
+    prefixSum = [0]
+    for i in range(m):
+        prefixSum.append(prefixSum[-1] + A[i])
+
+    for i in B:
+        index = bisect_left(prefixSum, i)
+        if index > m:
+            print("Go away!")
+        else:
+            print(index)
+
+
+if __name__ == "__main__":
+    solve()
